804c68a0ec4b19a3733704ede6f11bea91a77367
aproposmath <aproposmath@proton.me>
2026-02-09 22:09:11 +0100

    Allow WorkshopHandle instead of ModID in About.xml

--- build_modrepo.py
+++ build_modrepo.py
@@ -42,8 +42,11 @@ class ModMetadata:
             return el.strip()
 
         data = ModMetadata.read_data(elem)
+        id = elem.findtext("ModID") or elem.findtext("WorkshopHandle")
+        if id is None:
+            raise ValueError("Missing required ModID or WorkshopHandle")
         return ModMetadata(
-            id=get_field("ModID"),
+            id=id.strip(),
             version=get_field("Version"),
             name=get_field("Name"),
             author=get_field("Author"),
@@ -196,7 +199,7 @@ def handle_asset(asset: dict, cache: dict, all_zip_digests: set) -> ModMetadata 
         return mm
 
     # Download asset to temp dir using requests
-    outdir = Path("_downloads") / f"asset_{digest.replace(":", "_")}"
+    outdir = Path("_downloads") / f"asset_{digest.replace(':', '_')}"
     outdir.mkdir(parents=True, exist_ok=True)
 
     zip_path = outdir / name
